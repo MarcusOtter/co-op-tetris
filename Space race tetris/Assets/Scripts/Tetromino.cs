68525147b396b474c3aac7db68edf16edfdef183
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-02-26 10:13:47 +0100

    Add tetrominoes

---
+++ Space race tetris/Assets/Scripts/Tetromino.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    [SerializeField] private float _stepDistance = 1f;
+    [SerializeField] private float _stepDelay = 2f;
+
+    private bool _isFalling = true;
+
+    private void Awake()
+    {
+        StartCoroutine(TempSteps());
+    }
+
+    private IEnumerator TempSteps()
+    {
+        while (_isFalling)
+        {
+            yield return new WaitForSeconds(_stepDelay);
+            transform.position = new Vector3(transform.position.x, transform.position.y - _stepDistance, 0);           
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isFalling = false;
+    }
+}
